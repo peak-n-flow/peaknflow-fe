85c4f690ee399194de21f73ff53184034142ca2d
kenziehh <ghaisani.nurani@gmail.com>
2025-03-02 01:13:35 +0700

    `style` : setup container tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+	container:{
+		center: true,
+		padding: {
+			DEFAULT: '1rem',
+			sm: '2rem',
+			lg: '3rem',
+			xl: '4rem',
+			"2xl": '5rem',
+		},
+	},
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
