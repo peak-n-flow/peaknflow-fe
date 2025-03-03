3c91d0fc7322b36127ab2fb34c7c78b6e50c019b
kenziehh <ghaisani.nurani@gmail.com>
2025-03-03 15:53:09 +0700

    `hotfix` : fix eslint build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,4 @@
 import { authOptions } from "@/lib/auth-options";
 import NextAuth from "next-auth";
-import { NextRequest } from "next/server";
 
-interface RouteHandlerContext {
-  params: { nextauth: string[] };
-}
-
-const handler = async (req: NextRequest, context: RouteHandlerContext) => {
-  return NextAuth(req, context, authOptions);
-};
-
-export { handler as GET, handler as POST };
+export const { GET, POST } = NextAuth(authOptions);
