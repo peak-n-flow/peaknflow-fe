84522b7e069e18cfe0bc6913f8b6e50566b6644e
kenziehh <ghaisani.nurani@gmail.com>
2025-03-03 23:50:06 +0700

    `fix` : fix error session nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
 import { authOptions } from "@/lib/auth-options";
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 
-export const { GET, POST } = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
