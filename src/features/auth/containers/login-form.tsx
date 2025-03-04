fbb000c22ffe3df77d77a00cc099bda058301f33
kenziehh <ghaisani.nurani@gmail.com>
2025-03-04 11:04:49 +0700

    `feat` : pass login error message

--- src/features/auth/containers/login-form.tsx
+++ src/features/auth/containers/login-form.tsx
@@ -55,9 +55,14 @@ export default function LoginPage() {
         toast.success("Login successful.");
         router.push("/booking");
       } else {
-        throw response?.error || "Authentication failed";
+        throw response?.error as string;
       }
     } catch (error) {
+      const errorMessage =
+        typeof error === "string"
+          ? error
+          : "An error occurred. Please try again.";
+      toast.error(errorMessage);
       const errorMessage =
         typeof error === "string"
           ? error
@@ -69,74 +74,42 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex bg-white rounded-2xl px-4 md:px-10 py-10 md:py-20 flex-col justify-center space-y-6 min-w-72 sm:min-w-88 md:min-w-[440px] xl:min-w-[480px] 2xl:min-w-[520px] w-full xl:w-fit my-16 md:my-0 z-20">
-      <div className="flex flex-col space-y-2 text-center">
-        <h1 className="text-h1 font-medium">Login Account</h1>
-        <p className="text-body-lg text-light-100">
-          Enter your details below to login
-        </p>
-      </div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    type="email"
-                    placeholder="Enter your email"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="Enter your password"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full" disabled={isLoading}>
-            {isLoading ? "Logging in..." : "Login"}
-          </Button>
-        </form>
-      </Form>
-      <div className="flex gap-4 items-center">
-        <hr className="w-full h-0.5 bg-light-100" />
-        <span className="text-xs text-light-100">Or</span>
-        <hr className="w-full h-0.5 bg-light-100" />
-      </div>
-      {/* <Button variant="light" size="default" className="w-full">
-          <Image
-            alt="google"
-            src={GoogleIcon || "/placeholder.svg"}
-            width={24}
-            height={24}
-          />
-          Continue with Google
-        </Button> */}
-      <p className="px-8 text-center text-sm text-light-100">
-        Not registered yet?{" "}
-        <Link href="/auth/register" className="text-primary-60">
-          Create an Account
-        </Link>
-      </p>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 ">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Enter your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter your password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full" disabled={isLoading}>
+          {isLoading ? "Logging in..." : "Login"}
+        </Button>
+      </form>
+    </Form>
   );
 }
