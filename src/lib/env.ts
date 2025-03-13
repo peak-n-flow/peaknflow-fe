e7d8d607c9908336474c2ef59ce9a5f1ba6f672c
kenziehh <ghaisani.nurani@gmail.com>
2025-03-13 12:16:14 +0700

    feat: add environment variables for gym and service IDs

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,3 +2,7 @@ export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY
 export const NEXTAUTH_URL = process.env.NEXTAUTH_URL
+export const GYM_ID = process.env.NEXT_PUBLIC_GYM_ID
+export const SERVICE_GYM_ID = process.env.NEXT_PUBLIC_SERVICE_GYM_ID
+export const SERVICE_YOGA_ID = process.env.NEXT_PUBLIC_SERVICE_YOGA_ID
+export const SERVICE_RECOVERY_ID = process.env.NEXT_PUBLIC_SERVICE_RECOVERY_ID
