c4cc4f11eb93498565f85ea6cf99e16abd98d5c4
kenziehh <ghaisani.nurani@gmail.com>
2025-03-20 15:07:04 +0700

    `feat`: enhance accordion component with toggle icons and improved FAQ layout

--- src/features/service/containers/faq-container.tsx
+++ src/features/service/containers/faq-container.tsx
@@ -9,7 +9,7 @@ import type { FAQ } from "../data/faq";
 export default function FAQContainer({ faqs }: { faqs: FAQ[] }) {
   return (
     <section className="bg-[#1A1A1A] text-white py-16 md:py-24">
-      <div className="container flex flex-col gap-10 md:gap-20">
+      <div className="container flex flex-col gap-10">
         <h1>FREQUENTLY ASK QUESTION</h1>
         <Accordion
           type="single"
@@ -18,8 +18,8 @@ export default function FAQContainer({ faqs }: { faqs: FAQ[] }) {
         >
           {faqs.map((faq, index) => (
             <AccordionItem key={index} value={`item-${index}`}>
-              <AccordionTrigger className="text-h6 md:text-display-sm text-start items-start font-light">
-                <div className="flex items-start gap-4 md:gap-16 lg:gap-24 xl:gap-32">
+              <AccordionTrigger className="text-h6 md:text-display-xs xl:text-display-sm text-start items-start font-light pl-0 md:pl-[10vh] xl:pl-[20vh]">
+                <div className="flex gap-4 md:gap-8 lg:gap-24 xl:gap-32">
                   <span className="italic text-gray-500">
                     {index + 1 < 10 ? `0${index + 1}` : index + 1}
                   </span>
@@ -27,7 +27,7 @@ export default function FAQContainer({ faqs }: { faqs: FAQ[] }) {
                 </div>
               </AccordionTrigger>
               <AccordionContent className="mt-6 md:mt-20">
-                <div className="max-w-screen-md text-white/80 text-body-lg md:text-h6 pl-10 md:pl-16">
+                <div className=" text-white/80 text-body-lg md:text-h6 pl-0 md:pl-[17vh] lg:pl-[24vh] xl:pl-[39vh]">
                   {faq.answer}
                 </div>
               </AccordionContent>
